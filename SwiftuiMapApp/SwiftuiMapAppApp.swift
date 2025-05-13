2d493110d6eb89942b07a0deab6569e299027e4f
EugeniaUvarov <eugenia.uvarov@icloud.com>
2025-05-13 15:21:41 -0700

    Initial Commit

---
+++ SwiftuiMapApp/SwiftuiMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuiMapAppApp.swift
+//  SwiftuiMapApp
+//
+//  Created by Eugenia Uvarov on 5/13/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuiMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
