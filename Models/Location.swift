816943ab5e8994130c51aa2f4cd0a931d0a455c7
EugeniaUvarov <eugenia.uvarov@icloud.com>
2025-05-13 15:59:46 -0700

    Created Location Model

---
+++ Models/Location.swift
@@ -0,0 +1,21 @@
+//
+//  Location.swift
+//  SwiftuiMapApp
+//
+//  Created by Eugenia Uvarov on 5/13/25.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    
+    let name: String
+    let cityName: String
+    let ccoordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+    
+}
+
