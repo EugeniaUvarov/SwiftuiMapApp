4c01ce275cfae55b3db4537b026ed1258e157c69
EugeniaUvarov <eugenia.uvarov@icloud.com>
2025-05-20 10:27:58 -0700

    Finilized Mapview with Logic

--- Views/LocationsView.swift
+++ Views/LocationsView.swift
@@ -17,6 +17,12 @@ struct LocationsView: View {
         ZStack {
             Map(coordinateRegion: $vm.mapRegion)
                 .ignoresSafeArea()
+            VStack(spacing: 0) {
+                header
+                    .padding()
+        
+                Spacer()
+            }
         }
     }
 }
@@ -25,4 +31,37 @@ struct LocationsView: View {
     LocationsView()
         .environmentObject(LocationsViewModel())
       
+}
+
+extension LocationsView {
+    
+    private var header: some View {
+        
+        VStack {
+            Button(action: vm.toggleLocationsList) {
+                Text(vm.mapLocation.name + ", " +
+                     vm.mapLocation.cityName)
+                .font(.title2)
+                .foregroundStyle(.black)
+                .fontWeight(.semibold)
+                .frame(height: 55)
+                .frame(maxWidth: .infinity)
+                .animation(.none, value: vm.showLocationsList)
+                .overlay(alignment: .leading) {
+                    Image(systemName: "arrow.down")
+                        .font(.headline)
+                        .foregroundStyle(.primary)
+                        .padding()
+                        .rotationEffect(Angle(degrees: vm.showLocationsList ? 180 : 0 ))
+                }
+            }
+            if vm.showLocationsList {
+                LocationsListView()
+            }
+          
+        }
+        .background(.thickMaterial)
+        .cornerRadius(10)
+        .shadow(color: Color.black.opacity(0.3), radius: 20, x: 0, y: 15)
+    }
 }
